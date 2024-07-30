a9f317b2cbdde77640da841ccd356e43f058bb2e
ryuyroungkeoung <yok1812@naver.com>
2024-07-30 16:48:29 +0900

    포폴업로드

---
+++ js/common.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let flatpickr1 = document.querySelector(".flatpickr1");
+
+flatpickr1.flatpickr({
+  wrap: true,
+  mode: "multiple",
+  dateFormat: "Y-m-d",
+  defaultDate: ["2024-07-04", "2024-07-24"],
+});
+
+let flatpickr2 = document.querySelector(".flatpickr2");
+
+flatpickr2.flatpickr({
+  wrap: true,
+  mode: "multiple",
+  dateFormat: "Y-m-d",
+  defaultDate: ["2024-07-04", "2024-07-24"],
+});
+//
+let flatpickr3 = document.querySelector(".flatpickr3");
+
+flatpickr3.flatpickr({
+  wrap: true,
+  mode: "multiple",
+  dateFormat: "Y-m-d",
+  defaultDate: ["2024-07-04", "2024-07-24"],
+});
+
+let flatpickr4 = document.querySelector(".flatpickr4");
+
+flatpickr4.flatpickr({
+  wrap: true,
+  mode: "multiple",
+  dateFormat: "Y-m-d",
+  defaultDate: ["2024-07-04", "2024-07-24"],
+});
