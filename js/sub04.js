72ec8e675ae2428ea11da27ea407515286e29a51
ryuyroungkeoung <yok1812@naver.com>
2024-08-27 18:19:52 +0900

    자바스크립트 제이쿼리 중료

---
+++ js/sub04.js
@@ -0,0 +1,15 @@
+// 제이쿼리 사람들
+// $(document).ready(function () {
+//   $(".hum_warp").slick({
+//     dots: false,
+//     pauseOnHover: true,
+//     infinite: true,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     speed: 1500,
+//     draggable: true,
+//     slidesToShow: 3, // 한 번에 보여줄 슬라이드 갯수
+//   });
+// });
+
+// 제이쿼리 커뮤 글 2개 이상
